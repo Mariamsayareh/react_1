3c810300ff537edbe28f3e67f2b7cb243d420360
Mariamsayareh <mariamsayarah@gmail.com>
2025-10-27 14:48:40 +0200

    create Portfolio component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from './components/navbar/Navbar.jsx';
 import Hero from './components/hero/Hero.jsx';
+import Portfolio from './components/portfolio/Portfolio.jsx';
 function App(){
   return<>
   <Navbar/>
   <Hero/>
+  <Portfolio/>
   </>
 }
 export default App;
---
+++ src/components/portfolio /Portfolio.jsx
@@ -0,0 +1,12 @@
+import style from './portfolio.module.css';
+//import heroImg from './../../assets/img/hero.webp'
+
+function Portfolio() {
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default Portfolio;
