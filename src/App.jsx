11585ba9b675cbe42da92acde75cae5533a1c613
Mariamsayareh <mariamsayarah@gmail.com>
2025-10-27 19:08:34 +0200

    create a contact component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Navbar from './components/navbar/Navbar.jsx';
 import Hero from './components/hero/Hero.jsx';
 import Portfolio from './components/portfolio /Portfolio.jsx';
 import About from './components/about/About.jsx';
+import Contact from './components/contact/Contact.jsx'
 function App(){
   return<>
   <Navbar/>
   <Hero/>
   <Portfolio/>
   <About/>
+  <Contact/>
   </>
 }
 export default App;
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,12 @@
+import style from './contact.module.css';
+
+
+function Contact() {
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default Contact;
