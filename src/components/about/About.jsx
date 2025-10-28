fc5bc139690795b052301381be32855d9040028f
Mariamsayareh <mariamsayarah@gmail.com>
2025-10-28 22:22:50 +0200

    Responsive design adjustments

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,15 +5,19 @@ function About() {
     <>
         <div className={`${style.bgMain} d-flex flex-column justify-content-center align-items-center py-5 mb-5`}>
             <h2 className={`${style.about} text-center fw-bold text-uppercase py-5 text-white`}>about</h2>
-            <div className="pb-4 row row-cols-1 row-cols-md-2 g-4 d-flex justify-content-center">
-                <div className="col w-25">
+            <div className="px-3 pb-4 row row-cols-1 row-cols-md-4 g-4 d-flex justify-content-center">
+                 <div className="col ps-5">
+                </div>
+                <div className="col ps-5">
                     <p className={`text-white fs-5`}>Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
                 </div>
-                <div className="col w-25">
+                <div className="col ps-5">
                     <p className={`text-white fs-5`}>You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p>
                 </div>
+                 <div className="col ps-5">
+                </div>
             </div>
-            <div lassName={` `}>
+            <div>
             <button className={`${style.actve} btn border border-light py-2 px-4`}><i className={`${style.actveText} bi bi-download text-white pe-2`}></i> <span className={`${style.actveText} text-white`}>Free Download!</span></button>
             </div>
         </div>
