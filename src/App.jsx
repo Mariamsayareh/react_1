a93d9a50774621e6fd29245ae28b11ce66815fd2
Mariamsayareh <mariamsayarah@gmail.com>
2025-10-31 13:23:21 +0200

    add router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+import { Route,Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar.jsx';
 import Hero from './components/hero/Hero.jsx';
 import Portfolio from './components/portfolio /Portfolio.jsx';
 import About from './components/about/About.jsx';
 import Contact from './components/contact/Contact.jsx';
 import Footer from './components/footer/Footer.jsx';
+
 function App(){
   return<>
   <Navbar/>
-  <Hero/>
-  <Portfolio/>
-  <About/>
-  <Contact/>
+  <Routes>
+    <Route path='/' element={<Hero/>} ></Route>
+    <Route path='/Portfolio' element={<Portfolio/>} ></Route>
+    <Route path='/About' element={<About/>} ></Route>
+    <Route path='/Contact' element={<Contact/>} ></Route>
+  </Routes>
   <Footer/>
   </>
 }
